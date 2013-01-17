ac0895af18c4414409cb73e1c7f81ed2440415ce
Tom Anderson <tom.h.anderson@gmail.com>
2013-01-16 21:23:51 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+Zippopotamus PHP Library
+================================
+This is a library to abstract the Zippopotamus API addEvent method
+
+Installation
+------------
+  1. edit `composer.json` file with following contents:
+
+     ```json
+     "require": {
+        "zippopotamus/zippopotamus": "dev-master"
+     }
+     ```
+  2. install composer via `curl -s http://getcomposer.org/installer | php` (on windows, download
+     http://getcomposer.org/installer and execute it with PHP)
+  3. run `php composer.phar install`
+
+Use
+---
+
+```php
+use Zippopotamus\Service\Zippopotamus;
+
+$result = Zippopotamus::postalCode($countryCode, $postalCode);
+
+$result = Zippopotamus::place($countryCode, $state, $city);
+
+$result = Zippopotamus::nearby($countryCode, $postalCode);
+```
---
+++ src/Zippopotamus/Service/Zippopotamus.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Zippopotam.us PHP Library
+ *
+ * @version 1.0.0
+ */
+
+namespace Zippopotamus\Service;
+
+use Zend\Http\Client;
+use Zend\Http\Headers;
+use Zend\Json\Json;
+
+final class Zippopotamus
+{
+    static public function code($countryCode, $postalCode)
+    {
+        $http = new Client();
+
+        $http->setOptions(array('sslverifypeer' => false));
+        $headers = new Headers();
+        $headers->addHeaderLine('Content-Type', 'application/json');
+        $http->setHeaders($headers);
+
+        $http->setUri('http://api.zippopotam.us/' . urlencode($countryCode) . '/' . urlencode($postalCode));
+        $http->setMethod('POST');
+        $http->getRequest()->setContent(Json::encode($parameters));
+
+        $response = $http->send();
+        $json = Json::decode($response->getBody());
+
+        return $json;
+    }
+
+    static public function place($countryCode, $stateCode, $city)
+    {
+        $http = new Client();
+
+        $http->setOptions(array('sslverifypeer' => false));
+        $headers = new Headers();
+        $headers->addHeaderLine('Content-Type', 'application/json');
+        $http->setHeaders($headers);
+
+        $http->setUri('http://api.zippopotam.us/'
+            . urlencode($countryCode) . '/'
+            . urlencode($stateCode) . '/'
+            . urlencode($city)
+            );
+        $http->setMethod('POST');
+        $http->getRequest()->setContent(Json::encode($parameters));
+
+        $response = $http->send();
+        $json = Json::decode($response->getBody());
+
+        return $json;
+    }
+
+    static public function nearby($countryCode, $postalCode)
+    {
+        $http = new Client();
+
+        $http->setOptions(array('sslverifypeer' => false));
+        $headers = new Headers();
+        $headers->addHeaderLine('Content-Type', 'application/json');
+        $http->setHeaders($headers);
+
+        $http->setUri('http://api.zippopotam.us/nearby/' . urlencode($countryCode) . '/' . urlencode($postalCode));
+        $http->setMethod('POST');
+        $http->getRequest()->setContent(Json::encode($parameters));
+
+        $response = $http->send();
+        $json = Json::decode($response->getBody());
+
+        return $json;
+    }
+}
